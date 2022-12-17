ddf8bb7b623a7279e2616a7a755a542295492c73
Miumen <sharifigiv5954@gmail.com>
2022-12-17 09:38:14 +0330

    add parabolic

--- transform.cpp
+++ transform.cpp
@@ -12,3 +12,14 @@ int Line::get_y(float x){
     return(m * x + b);
 };
 
+
+class Line2 {
+    public:
+        int get_y(float x);
+        float a, b, c;
+    
+};
+
+int Line2::get_y(float x){
+    return(a * x * x + b * x + c);
+};
